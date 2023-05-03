a6201f824f4d2fe8893a4dae63572aec1cd562a5
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-05-03 19:08:00 +0200

    Fix: connection failure due to misspelled words

--- app/worker.py
+++ app/worker.py
@@ -1,6 +1,6 @@
 import time
 import random
-from app import tasks_collection
+from app.database import tasks_collection
 from pymongo import MongoClient, ReturnDocument
 
 # Function stimulates task processing and update task status in MongoDB
@@ -11,8 +11,8 @@ def perform_task(task_id, data):
 
     # Update task status and result in MongoDB
     result = "Task completed in {} seconds.".format(processing_time)
-    task_collection.find_one_and_update(
-        {'_id': MongoClient().task_queue.tasks_collection.ObjectId(ask_id)},
+    tasks_collection.find_one_and_update(
+        {'_id': MongoClient().task_queue.tasks_collection.ObjectId(task_id)},
         {'$set': {'status': 'completed', 'result': result}},
         return_document=ReturnDocument.AFTER
     )
