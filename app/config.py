474b26e43103649ac6a86bdff6938b7c93e0b229
Eskabore <turquinjl@gmail.com>
2023-05-06 20:45:40 +0200

    Config .env

--- app/config.py
+++ app/config.py
@@ -5,8 +5,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Redis settings
-    REDIS_HOST = os.environ.get('REDIS_HOST') or 'localhost' or '12345678'
-    REDIS_PORT = int(os.environ.get('REDIS_PORT') or 6379)
+    REDIS_HOST = os.environ.get('REDIS_HOST') or '3000'
+    REDIS_PORT = int(os.environ.get('REDIS_PORT'))
 
     # MongoDB settings
     MONGODB_CONNECTION_STRING = os.environ.get('MONGODB_CONNECTION_STRING')
