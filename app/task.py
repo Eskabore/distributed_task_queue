09547d266d2523ae6b2a8fcdd1a4ecf950b409be
Eskabore <turquinjl@gmail.com>
2023-05-08 19:50:03 +0200

    Newe class for field update

--- app/task.py
+++ app/task.py
@@ -1,7 +1,7 @@
 # Description: This file contains the task related functions
 from bson.objectid import ObjectId
 from app.database import tasks_collection
-
+from datetime import datetime
 
 def update_task_status(task_id, status):
     # Update the task status based on the given task_id
@@ -11,3 +11,23 @@ def update_task_status(task_id, status):
     )
 
     print(f"Task {task_id} updated to status: {status}")
+
+class Task:
+    def __init__(self, input_data):
+        self.input_data = input_data
+        self.status = "pending"
+        self.result = None
+        self.created_at = datetime.now()
+
+    def save(self):
+        task_data = {
+            "input_data": self.input_data,
+            "status": self.status,
+            "result": self.result,
+            "created_at": self.created_at
+        }
+        result = tasks_collection.insert_one(task_data)
+        self.id = str(result.inserted_id)
+
+    def update_status(self):
+        update_task_status(self.id, self.status)
