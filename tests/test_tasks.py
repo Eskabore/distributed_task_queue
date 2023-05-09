3ab913b13b49d0b8f87386de9d1e8b61da80f328
Eskabore <turquinjl@gmail.com>
2023-05-09 12:53:18 +0200

    Updated tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,14 +2,16 @@ import json
 from bson import ObjectId
 from app import app
 from app.database import tasks_collection
+import re
 
 def test_index():
     with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
-        assert response.data == b'Welcome to the Distributed Task Queue API!'
+        assert b'Welcome to the Distributed Task Queue' in response.data
 
 def test_create_task():
+    app.secret_key = 'testing_key'
     with app.test_client() as client:
         new_task = {
             'description': 'Test task',
@@ -17,11 +19,13 @@ def test_create_task():
             'task_type': 'data_cleaning'
         }
 
-        response = client.post('/tasks', json=new_task)
-        assert response.status_code == 201
-        assert 'task_id' in json.loads(response.data)
+        response = client.post('/tasks/create', data=new_task)
+        assert response.status_code == 302
 
-        task_id = json.loads(response.data)['task_id']
+        # Extract the task ID from the redirection URL
+        task_id = re.search(r'/tasks/([\w\d]+)', response.location).group(1)
+
+        # Clean up the test task
         tasks_collection.delete_one({'_id': ObjectId(task_id)})
 
 def test_get_task():
@@ -37,8 +41,19 @@ def test_get_task():
         # Get the created task
         response = client.get(f'/tasks/{task_id}')
         assert response.status_code == 200
-        assert json.loads(response.data)['description'] == 'Test task'
+        assert b'Test task' in response.data
 
         # Clean up the test task
         tasks_collection.delete_one({'_id': ObjectId(task_id)})
+
+def test_get_all_tasks():
+    with app.test_client() as client:
+        response = client.get('/tasks/view')
+        assert response.status_code == 200
+
+def test_search_tasks():
+    with app.test_client() as client:
+        response = client.get('/tasks/view?search=Test')
+        assert response.status_code == 200
+        assert b'Test task' in response.data
 
